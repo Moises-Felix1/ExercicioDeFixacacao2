85a284dda02e5b2a2bab4f37a6e4ff350e1684a3
Moises Felix <moises012felix@gmail.com>
2024-03-06 10:27:23 -0300

    Criado o projeto principal, feito a entrada de dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,10 @@ package application;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 import entities.ImportedProduct;
 import entities.Product;
@@ -10,21 +14,66 @@ import entities.UsedProduct;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		Scanner sc = new Scanner(System.in);
 		
-
-		/*ImportedProduct p = new ImportedProduct("Tablet", 260.00, 20.00);
-		System.out.println(p.priceTag());*/
+		List<Product> listap = new ArrayList<>();
 		
-		/*Product p = new Product("Tablet", 260.00);
-		System.out.println(p.priceTag());*/
+		System.out.print("Enter the number of products: ");
+		int number = sc.nextInt();
 		
-		String d = "18/07/2022";
-		LocalDate data = LocalDate.parse(d, fmt);
+		for(int i = 0; i < number; i++) {
+			System.out.println("Product #"+(i+1)+" data:");
+			System.out.print("Common, used or imported(c/u/i)? ");
+			char answer = sc.next().charAt(0);
+			if(answer == 'c') {
+				System.out.print("Name: ");
+				sc.nextLine();
+				String name = sc.next();
+				System.out.print("Price: ");
+			    double price = sc.nextDouble();
+				listap.add(new Product(name, price));
+				System.out.println();
+			}else {
+				if(answer == 'u') {
+					System.out.print("Name: ");
+					sc.nextLine();
+					String name = sc.next();
+					System.out.print("Price: ");
+				    double price = sc.nextDouble();
+					System.out.print("Manufacture date (DD/MM/YYYY): ");
+					sc.nextLine();
+					String data = sc.nextLine();
+					LocalDate dte = LocalDate.parse(data, fmt);
+					listap.add(new UsedProduct(name, price, dte));
+					System.out.println();
+				}else {
+					if(answer == 'i') {
+						System.out.print("Name: ");
+						sc.nextLine();
+						String name = sc.next();
+						System.out.print("Price: ");
+						double price = sc.nextDouble();
+						System.out.print("Customs fee: ");
+						double tax = sc.nextDouble();
+						listap.add(new ImportedProduct(name, price, tax));
+						System.out.println();
+					}
+				}
+			}
+		}
 		
-		UsedProduct p = new UsedProduct("Iphone", 400.00, data);
-		System.out.println(p.priceTag());
+		System.out.println();
+		
+		System.out.println("PRICE TAGS:");
+		for(Product p : listap) {
+			System.out.println(p.priceTag());
+		}
+		
+		sc.close();
+		
 	}
-
+	
 }
